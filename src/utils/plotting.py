a76883e2deeb534cbf20be27e348d91837461d03
michelebanfi <michi.banfi01@gmail.com>
2026-01-20 14:48:41 +0100

    better plotting

--- src/utils/plotting.py
+++ src/utils/plotting.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 
 def plot_simulation_results(results, filename="simulation_results.png"):
+    colors = ["#2E72AE", "#64B791", "#DBA142", "#000000", "#E17792"]
     plt.figure(figsize=(10, 7))
-    for name, data in results.items():
+    for i, (name, data) in enumerate(results.items()):
         ps = sorted(data.keys())
         lers = [data[p]['logical_error_rate'] for p in ps]
-        plt.loglog(ps, lers, 'o-', label=f"n={name}")
+        plt.loglog(ps, lers, 'o-', label=f"n={name}", color=colors[i % len(colors)])
     
     plt.xlabel("Physical Error Rate p")
     plt.ylabel("Logical Error Rate LER")
